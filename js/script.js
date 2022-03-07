8c77fb60d1a5c0d0b978a155ee700dc7e3681e3b
Neeiser <stortini.study@gmail.com>
2022-03-07 15:22:23 +0100

    Basic structure

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/* 
+- Come prima cosa nel file js definitevi un array di oggetti che 
+rappresentano i membro del team.
+
+- Ogni membro deve avere le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
+
+- Prendendo come riferimento la card di esempio presente nell'html, 
+stampiamo dinamicamente una card per ogni membro del team. 
+*/
+
+const objArr = [
+    {
+        name: '',
+        role: '',
+        photo: '',
+    }
+]
